a7079fbfaaa365828c063e78f149ec85e08551c1
Cristiane Pagine <cristianepagine@gmail.com>
2024-01-21 11:07:41 -0300

    Enums
    Enums

---
+++ HelpDesk/src/test/java/com/cristiane/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristiane.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
