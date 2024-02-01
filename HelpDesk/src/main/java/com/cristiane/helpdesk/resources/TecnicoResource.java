a24625e804e22b3bcd808163c37e56ccf12ef57f
Cristiane Pagine <cristianepagine@gmail.com>
2024-02-01 15:09:30 -0300

    endpoint findall to tecnician

--- HelpDesk/src/main/java/com/cristiane/helpdesk/resources/TecnicoResource.java
+++ HelpDesk/src/main/java/com/cristiane/helpdesk/resources/TecnicoResource.java
@@ -10,9 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
-public class  TecnicoResource {
+public class TecnicoResource {
 
     @Autowired
     private TecnicoService tecnicoService;
@@ -23,5 +26,12 @@ public class  TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico obj = tecnicoService.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll() {
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
